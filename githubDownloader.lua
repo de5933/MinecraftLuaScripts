3827d0a768d2009c254e03e133b39cacc7788aec
Daragh <de5933@gmail.com>
2020-08-23 18:41:38 -0400

    allow full overriding of path

--- githubDownloader.lua
+++ githubDownloader.lua
@@ -106,7 +106,7 @@ function downloadManager( path )
                 fPath[i] = fPath[i].."/"..fName[i]
             end
             local path = fPath[i]
-            if gPath ~= "" then path = gPath.."/"..gRepo.."/"..fPath[i] end
+            if gPath ~= "" then path = gPath.."/"..fPath[i] end
             if not fileList.files[path] and not isBlackListed(fPath[i]) then
                 fileList.files[path] = {"https://raw.github.com/"..gUser.."/"..gRepo.."/"..gBranch.."/"..fPath[i],fName[i]}
             end
@@ -115,7 +115,7 @@ function downloadManager( path )
     for i, data in pairs(fType) do
         if data == "dir" then
             local path = fPath[i]
-            if gPath ~= "" then path = gPath.."/"..gRepo.."/"..fPath[i] end
+            if gPath ~= "" then path = gPath.."/"..fPath[i] end
             if not fileList.dirs[path] then 
                 writeCenter("Listing directory: "..fName[i])
                 fileList.dirs[path] = {"https://raw.github.com/"..gUser.."/"..gRepo.."/"..gBranch.."/"..fPath[i],fName[i]}
