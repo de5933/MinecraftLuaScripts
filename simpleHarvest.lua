154a7063fc31885742138829c4ac377b102883fa
Daragh <de5933@gmail.com>
2020-08-25 20:40:22 -0400

    added some utilities and simple harvest

---
+++ simpleHarvest.lua
@@ -0,0 +1,17 @@
+-- Very simple harvest program
+-- Watch the block below
+-- If a block is detected, mine it
+-- Drop results above (ideally into a chest)
+
+while true do
+	if turtle.detectDown() then
+		turtle.digDown()
+		for i = 1, 16 do
+			if turtle.getItemCount(i) > 0 then
+				turtle.select(i)
+				turtle.dropUp()
+			end
+		end
+	end
+	sleep(1)
+end
